fbe0f414173c58c308e18bac14b200aada7c9e00
Stephen Caulfield <stephencawfield@live.ie>
2020-05-13 19:19:13 +0100

    Add files via upload

---
+++ AccuracyCalc.java
@@ -0,0 +1,25 @@
+import javax.swing.JOptionPane;
+
+public class AccuracyCalc {
+
+    public static void main(String[] args) {
+        // Prints "Hello, World" to the terminal window.
+        double A;
+		double a;
+		double ad;
+		double aa;
+		double E;
+		String B = JOptionPane.showInputDialog("Affinity" );
+		String C = JOptionPane.showInputDialog("Accuracy");
+		String D = JOptionPane.showInputDialog("Enemy Defence");
+		String F = JOptionPane.showInputDialog("Enemy Armour");
+		A = Double.parseDouble(B);
+		a = Double.parseDouble(C);
+		ad = Double.parseDouble(D);
+		aa = Double.parseDouble(F);
+		E = ((0.0008*ad)*(0.0008*ad)*(0.0008*ad)+(4*ad)+40)+aa;
+		double G = A * (a/E);
+		JOptionPane.showMessageDialog(null, "Hit Chance: " + G);
+    }
+
+}
